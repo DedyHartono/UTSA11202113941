8821edeac68328a4af58ace7ae3c7b3f9f0a9553
Dedy hartono <97329001+DedyHartono@users.noreply.github.com>
2023-05-11 16:56:22 +0700

    Add files via upload

---
+++ MahasiswaAktif.java
@@ -0,0 +1,22 @@
+package UTS_A11202113941;
+
+public class MahasiswaAktif extends Mahasiswa{
+
+	public MahasiswaAktif(String nim, String nama, int semester, int usia, String[] krs) {
+		super(nim, nama, semester, usia, krs);
+		// TODO Auto-generated constructor stub
+	}
+
+	 	float hitungRataNilai(int[] nilai) {
+	        return super.hitungRataNilai(nilai);
+	    }
+
+	    void infoMahasiswa() {
+	       super.infoMahasiswa();
+	    }
+
+	    void infoKrsMahasiswa() {
+	        super.infoKrsMahasiswa();
+	    }
+}
+
